1d6764c542627bf25ea5aa5fe786133f5fc89b2d
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2021-02-01 16:21:31 +0100

    app.js

--- app.js
+++ app.js
@@ -1,58 +1,137 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const flash = require("connect-flash");
 
 mongoose
-  .connect('mongodb://localhost/petsapp-react', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .connect("mongodb://localhost/petsapp-react", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
-// Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true,
+  })
+);
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.locals.title = "PetsApp";
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const bcrypt = require("bcrypt");
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 * 1000,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const User = require("./models/User");
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((dbUser) => {
+      done(null, dbUser);
+      hbs.registerHelper("isid", function (value) {
+        return JSON.stringify(value) !== JSON.stringify(dbUser._id);
+      });
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      User.findOne({ username: username })
+        .then((found) => {
+          if (found === null) {
+            return done(null, false, { message: "Wrong credentials" });
+          }
+          if (!bcrypt.compareSync(password, found.password)) {
+            return done(null, false, { message: "Wrong credentials" });
+          }
+          done(null, found);
+        })
+        .catch((err) => {
+          done(err, false);
+        });
+    }
+  )
+);
 
 
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
-const index = require('./routes/index');
-app.use('/', index);
-
+const index = require("./routes/index");
+app.use("/", index);
+const auth = require("./routes/auth");
+app.use("/", auth);
+const pets = require("./routes/pets");
+app.use("/", pets);
+const users = require("./routes/users");
+app.use("/", users);
+const match = require("./routes/match");
+app.use("/", match);
+const message = require("./routes/message");
+app.use("/", message);
 
 module.exports = app;
