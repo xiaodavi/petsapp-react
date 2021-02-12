ded05a8600d2efc57d0fb8f6b23ca329cac96e4f
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2021-02-12 16:18:23 +0100

    pet data page

--- client/src/pages/PetData.js
+++ client/src/pages/PetData.js
@@ -1,47 +1,38 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import PetCard from '../components/PetCard'
+import styled from 'styled-components'
 
 const PetData = (props) => {
 
   const [pets, setPets] = useState([])
   console.log(pets)
-  const getPets = () => {
-    axios.get(`/api/pets/${props.user._id}/mypets`)
-    .then(res => {
+
+  useEffect(() => {
+      axios.get(`/api/pets/${props.user._id}/mypets`)
+      .then(res => {
       console.log(res.data)
       setPets(res.data)
-    })
-  }
+      });
+  }, [])
 
-
-
-  // useEffect(() => {
-  //   function getPets(status) {
-  //     axios.get(`/api/pets/${props.user._id}/mypets`)
-  //     .then(res => {
-  //     console.log(res.data)
-  //     setPets(res.data);
-  //   })
-  //   }
-  
-  //   // ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
-  //   // return () => {
-  //   //   ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
-  //   // };
-  // }, [props.user._id]); // Only re-subscribe if props.friend.id changes
+  const Wrapper = styled.div`
+    display: flex;
+    display-direction: column;
+    justify-content: center;
+    align-items: center
+  `
 
   return (
-    <div>
-        {pets.map(pet => {
-        <>
-        <img src={pet.petsimage} alt={pet.petsname} />
-        <p>{pet.petsname}</p>
-        <p>{pet.breed}</p>
-        </> 
-      })
-    })
-  </div>
- )
+    <>
+    <h1>My Pets</h1>
+     {pets.map(pet => <Wrapper>
+     <PetCard title={pet.petsname} image={pet.petsimage} desc={pet.breed}/>
+     </Wrapper>
+     )}
+    </>
+  )
+
 }
 
 export default PetData
