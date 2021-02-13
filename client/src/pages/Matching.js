56b16f2f53c4b8c0ae238c948dca3804eec89aae
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2021-02-13 23:52:31 +0100

    icons

--- client/src/pages/Matching.js
+++ client/src/pages/Matching.js
@@ -3,6 +3,7 @@ import PetCard from '../components/PetCard'
 import axios from 'axios'
 import styled from 'styled-components'
 import BottomNav from '../components/BottomNav'
+import { SmileTwoTone, HeartTwoTone } from '@ant-design/icons';
 
 const Matching = () => {
 
@@ -23,8 +24,8 @@ const Matching = () => {
   return (
     <div>
       <PetCard title={randomPet.petsname} image={randomPet.petsimage} desc={randomPet.breed}
-        buttomName1="Like"
-        buttomName2="Dislike"
+        buttomName1={<HeartTwoTone twoToneColor="#eb2f96" style={{ fontSize: '30px'}}/>}
+        buttomName2={<SmileTwoTone twoToneColor="#52c41a" rotate={180} style={{ fontSize: '30px'}}/>}
       />
       <BottomNav />
     </div>
