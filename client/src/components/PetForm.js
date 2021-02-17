840dd21bbf86f51e43b0ed65314ecfb42557bcc3
Xiaomei Wu <xiaomeiwu@Xiaomeis-MacBook-Air.local>
2021-02-17 22:30:20 +0100

    upload image style antd

--- client/src/components/PetForm.js
+++ client/src/components/PetForm.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { Input } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
+import { WomanOutlined } from '@ant-design/icons';
 
 const PetForm = (props) => {
 
   return (
     <div>
-      <label htmlFor="petsname">Name: </label>
-      <input id="petsname" name="petsname" value={props.petsname} 
-      onChange={props.handleChange} type="text" />
-      <label htmlFor="breed">Breed: </label>
-      <input id="breed" name="breed" value={props.breed} 
-      onChange={props.handleChange} type="text" />
+      {/* <label htmlFor="petsname">Name: </label> */}
+      <Input id="petsname" name="petsname" value={props.petsname} 
+      onChange={props.handleChange} type="text" placeholder="name" prefix={<SmileOutlined />} />
+      {/* <input id="petsname" name="petsname" value={props.petsname} 
+      onChange={props.handleChange} type="text" /> */}
+      {/* <label htmlFor="breed">Breed: </label> */}
+      <Input id="breed" name="breed" value={props.breed} 
+      onChange={props.handleChange} type="text" placeholder="breed" prefix={<WomanOutlined />} />
+      {/* <input id="breed" name="breed" value={props.breed} 
+      onChange={props.handleChange} type="text" /> */}
     </div>
   )
 }
